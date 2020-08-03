6fc3cb1c57d0e7391eda842d34068f01a7f3f755
linbo20110131 <493658400@qq.com>
2020-08-03 17:31:29 +0800

    ceshi

--- src/com/yr/test.java
+++ src/com/yr/test.java
@@ -3,7 +3,7 @@ package com.yr;
 public class test {
     public static void main(String[] args) {
         System.out.println("这是测试4号");
-        
+        System.out.println("平萝莉");
 
         System.out.println(32);
         System.out.println(3434);
